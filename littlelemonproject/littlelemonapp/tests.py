9289bb702d09738f4c9df66de6a8f9c43833e0f8
Alejandro Chavez Noguera <alejandro.chavez@konfio.mx>
2024-12-06 12:03:03 -0600

    Adding tests

--- littlelemonproject/littlelemonapp/tests.py
+++ littlelemonproject/littlelemonapp/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from .models import Booking, Menu
+from datetime import date
 
-# Create your tests here.
+class BookingModelTest(TestCase):
+
+    def setUp(self):
+        self.booking = Booking.objects.create(
+            first_name="John Doe",
+            reservation_date=date.today(),
+            reservation_slot=10,
+        )
+
+    def test_booking_str(self):
+        self.assertEqual(str(self.booking), "John Doe")
+
+    def test_booking_fields(self):
+        self.assertEqual(self.booking.first_name, "John Doe")
+        self.assertEqual(self.booking.reservation_date, date.today())
+        self.assertEqual(self.booking.reservation_slot, 10)
